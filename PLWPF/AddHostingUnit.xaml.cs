151d70094531a09e83f917c13d888a3e63c81bee
soriteigman <soriteigman@gmail.com>
2020-01-07 16:18:33 +0200

    rating not working

--- PLWPF/AddHostingUnit.xaml.cs
+++ PLWPF/AddHostingUnit.xaml.cs
@@ -22,9 +22,15 @@ namespace PLWPF
     /// </summary>
     public partial class AddHostingUnit : Page
     {
+        int intRate = 0;
+        int intCount = 1;
+        int Rate = 0;
         public AddHostingUnit()
         {
             InitializeComponent();
+
+            LoadImages();
+            lblRating.Text = intRate.ToString();
 
             this.cbArea.ItemsSource = Enum.GetValues(typeof(BE.VacationArea));
             this.cbSubArea.ItemsSource = Enum.GetValues(typeof(BE.VacationSubArea));
@@ -42,9 +48,89 @@ namespace PLWPF
 
         }
 
+        #region rating
+        private void LoadImages()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Image img = new Image();
+                img.Name = "imgRate" + i;
+                img.Stretch = Stretch.UniformToFill;
+                img.Height = 25;
+                img.Width = 25;
+                img.Source = new BitmapImage(new Uri(@"\Resources\MinusRate.png", UriKind.Relative));
+                img.MouseEnter += imgRateMinus_MouseEnter;
+                pnlMinus.Children.Add(img);
+
+                Image img1 = new Image();
+                img1.Name = "imgRate" + i + i;
+                img1.Stretch = Stretch.UniformToFill;
+                img1.Height = 25;
+                img1.Width = 25;
+                img1.Visibility = Visibility.Hidden;
+                img1.Source = new BitmapImage(new Uri(@"\Resources\PlusRate.png", UriKind.Relative));
+                img1.MouseEnter += imgRatePlus_MouseEnter;
+                img1.MouseLeave += imgRatePlus_MouseLeave;
+                img1.MouseLeftButtonUp += imgRatePlus_MouseLeftButtonUp;
+                pnlPlus.Children.Add(img1);
+            }
+        }
+
+        private void imgRateMinus_MouseEnter(object sender, MouseEventArgs e)
+        {
+            GetData(sender, Visibility.Visible, Visibility.Hidden);
+        }
+
+        private void imgRatePlus_MouseEnter(object sender, MouseEventArgs e)
+        {
+            GetData(sender, Visibility.Visible, Visibility.Hidden);
+        }
+
+        private void imgRatePlus_MouseLeave(object sender, MouseEventArgs e)
+        {
+            GetData(sender, Visibility.Hidden, Visibility.Visible);
+        }
+
+        private void gdRating_MouseLeave(object sender, MouseEventArgs e)
+        {
+            SetImage(Rate, Visibility.Visible, Visibility.Hidden);
+        }
+
+        private void GetData(object sender, Visibility imgYellowVisibility, Visibility imgGrayVisibility)
+        {
+            GetRating(sender as Image);
+            SetImage(intRate, imgYellowVisibility, imgGrayVisibility);
+        }
+
+        private void SetImage(int intRate, Visibility imgYellowVisibility, Visibility imgGrayVisibility)
+        {
+            foreach (Image imgItem in pnlPlus.Children.OfType<Image>())
+            {
+                if (intCount <= intRate)
+                    imgItem.Visibility = imgYellowVisibility;
+                else
+                    imgItem.Visibility = imgGrayVisibility;
+                intCount++;
+            }
+            intCount = 1;
+        }
+
+        private void imgRatePlus_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            GetRating(sender as Image);
+            Rate = intRate;
+            lblRating.Text = intRate.ToString();
+        }
+
+        private void GetRating(Image Img)
+        {
+            string strImgName = Img.Name;
+            intRate = Convert.ToInt32(strImgName.Substring(strImgName.Length - 1, 1));
+        }
+
+        #endregion
 
 
- 
         private void id_PreviewMouseDown(object sender, MouseButtonEventArgs e)//cleaning the text box so you can write the ID 
         {
             if (ID.Text == "Enter Your ID")
