040dbd7db6796dcdf5ec8afa1887906650527e7e
estiburack <stburack@gmail.com>
2020-01-09 16:00:22 +0200

    im first

--- PLWPF/GuestReq.xaml.cs
+++ PLWPF/GuestReq.xaml.cs
@@ -26,20 +26,21 @@ namespace PLWPF
         {
             InitializeComponent();
             this.type.ItemsSource = Enum.GetValues(typeof(BE.VacationType));
+            this.areacb.ItemsSource = Enum.GetValues(typeof(BE.VacationArea));
 
 
 
             startday.BlackoutDates.AddDatesInPast();
-            startday.BlackoutDates.Add(new CalendarDateRange(DateTime.Now.AddMonths(11),DateTime.Now.AddYears(100)));
+            startday.BlackoutDates.Add(new CalendarDateRange(DateTime.Now.AddMonths(11), DateTime.Now.AddYears(100)));
             endday.BlackoutDates.AddDatesInPast();
 
         }
         private void Startday_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
-        { 
+        {
             DateTime from = startday.SelectedDate.Value.Date;
             endday.BlackoutDates.Clear();
             endday.BlackoutDates.AddDatesInPast();
-            endday.BlackoutDates.Add((new CalendarDateRange(DateTime.Now,from)));
+            endday.BlackoutDates.Add((new CalendarDateRange(DateTime.Now, from)));
 
         }
 
@@ -49,10 +50,51 @@ namespace PLWPF
 
             DateTime from = startday.SelectedDate.Value.Date;
             DateTime to = endday.SelectedDate.Value.Date;
-            int numdays= (to - from).Days;
-            numofday.Content = numdays-1 + "-night stay";
+            int numdays = (to - from).Days;
+            numofday.Content = numdays - 1 + "-night stay";
         }
 
- 
+        private void Areacb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            areacb.Visibility = Visibility.Visible;
+            string val = areacb.SelectedValue.ToString();
+
+            if (val == "DeadSea")
+            {
+                subareacb.SelectedItem = "{Binding Path=DeadSea,Mode=TwoWay}";
+                this.subareacb.ItemsSource = Enum.GetValues(typeof(BE.DeadSea));
+            }
+            else if (val == "Eilat")
+            {
+                subareacb.SelectedItem = "{Binding Path=Eilat,Mode=TwoWay}";
+                this.subareacb.ItemsSource = Enum.GetValues(typeof(BE.Eilat));
+            }
+            else if (val == "Jerusalem")
+            {
+                subareacb.SelectedItem = "{Binding Path=Jerusalem,Mode=TwoWay}";
+                this.subareacb.ItemsSource = Enum.GetValues(typeof(BE.Jerusalem));
+            }
+            else if (val == "North")
+            {
+                subareacb.SelectedItem = "{Binding Path=North,Mode=TwoWay}";
+                this.subareacb.ItemsSource = Enum.GetValues(typeof(BE.North));
+            }
+            else if (val == "South")
+            {
+                subareacb.SelectedItem = "{Binding Path=South,Mode=TwoWay}";
+                this.subareacb.ItemsSource = Enum.GetValues(typeof(BE.South));
+            }
+            else if (val == "Center")
+            {
+                subareacb.SelectedItem = "{Binding Path=Center,Mode=TwoWay}";
+                this.subareacb.ItemsSource = Enum.GetValues(typeof(BE.Center));
+            }
+            else
+            {
+                subareacb.SelectedItem = "{Binding Path=All,Mode=TwoWay}";
+                this.subareacb.ItemsSource = Enum.GetValues(typeof(BE.All));
+            }
+        }
     }
 }
