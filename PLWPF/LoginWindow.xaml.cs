d8be4b430723fce4e47959403b0c2a687b79be8c
soriteigman <soriteigman@gmail.com>
2020-01-14 16:23:36 +0200

    tt

--- PLWPF/LoginWindow.xaml.cs
+++ PLWPF/LoginWindow.xaml.cs
@@ -19,16 +19,46 @@ namespace PLWPF
     /// </summary>
     public partial class LoginWindow : Window
     {
-        public LoginWindow()
+        public Page p ;
+
+        public LoginWindow(Page goTo)
         {
+            p = goTo;
             InitializeComponent();
         }
 
         private void login_Click(object sender, RoutedEventArgs e)
         {
+            //MessageBox.Show("Please Enter Admin Password", "This Page is Private", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+
             bool flag = true;
-            if (usernameBox.Password.ToString() != "Estiburack" || usernameBox.Password.ToString() != "estiburack" || usernameBox.Password.ToString() != "Soriteigman" || usernameBox.Password.ToString() != "soriteigman")
+            if (usernameBox.Text != "Estiburack" && usernameBox.Text != "estiburack" && usernameBox.Text != "Soriteigman" && usernameBox.Text != "soriteigman")
+            {
                 flag = false;
+
+            }
+            if (flag&&passwordBox.Password.ToString()!="7777")
+            {
+                flag = false;
+            }
+            if(!flag)
+            {
+               MessageBoxResult myResult= MessageBox.Show("Incorrect username or password", "ERROR", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+               if(myResult==MessageBoxResult.OK)
+                {
+                    usernameBox.Clear();
+                    passwordBox.Clear();
+                }
+                if (myResult == MessageBoxResult.Cancel)
+                    Close();
+            }
+            if (flag)
+            {
+                p.NavigationService.Navigate(new Admin());
+
+                Close();
+
+            }
 
 
         }
